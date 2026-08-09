521f9ec63e3646ff6024561bf52a3207614ceb8e
phoo3011 <phooriwat_s@cmu.ac.th>
2026-08-09 14:52:28 +0700

    Workshop #1

--- src/main/kotlin/Workshop1.kt
+++ src/main/kotlin/Workshop1.kt
@@ -21,6 +21,15 @@ fun main() {
         // เลือก 2 เพื่อแปลง Kilometers เป็น Miles: convertKilometersToMiles()
         // เลือก 'exit' เพื่อออกจากโปรแกรม
         // 🚨
+        when (choice) {
+            "1" -> convertCelsiusToFahrenheit()
+            "2" -> convertKilometersToMiles()
+            "exit" -> {
+                println("ออกจากโปรแกรม โชคดีมีชัยโชคชัยมีวัว!")
+                break
+            }
+            else -> println("ตัวเลือกไม่ถูกต้อง: กรุณาเลือก 1, 2 หรือพิมพ์ exit เพื่อออกจากโปรแกรม")
+        }
 
 
         println() // พิมพ์บรรทัดว่างเพื่อความสวยงาม
@@ -30,11 +39,17 @@ fun main() {
 // 4. สร้างฟังก์ชันแยกสำหรับการแปลงหน่วย Celsius to Fahrenheit: celsiusToFahrenheit
 // สูตร celsius * 9.0 / 5.0 + 32
 // 🚨
+fun celsiusToFahrenheit(celsius: Double): Double {
+    return celsius * 9.0 / 5 + 32.0
+}
 
 
 // 4. สร้างฟังก์ชันแยกสำหรับการแปลงหน่วย Kilometers to Miles: kilometersToMiles
 // สูตร kilometers * 0.621371
 // 🚨
+fun kilometersToMiles(kilometers: Double): Double {
+    return kilometers * 0.621371
+}
 
 
 // ฟังก์ชันสำหรับจัดการกระบวนการแปลง Celsius to Fahrenheit ทั้งหมด
@@ -46,13 +61,19 @@ fun convertCelsiusToFahrenheit() {
     // ออกจากฟังก์ชัน convertCelsiusToFahrenheit() หากข้อมูลผิดพลาด: return
     // celsius
     // 🚨
+    val celsius = input.toDoubleOrNull() ?: run {
+        println("ข้อผิดพลาด: กรุณาป้อนข้อมูลเป็นตัวเลขเท่านั้น")
+        return
+    }
 
 
 //🚨    val fahrenheitResult = celsiusToFahrenheit(celsius)
+    val fahrenheitResult = celsiusToFahrenheit(celsius)
 
     // 6. แสดงผลลัพธ์
     // ใช้ String format เพื่อแสดงทศนิยม 2 ตำแหน่ง
 //🚨    println("ผลลัพธ์: $celsius °C เท่ากับ ${"%.2f".format(fahrenheitResult)} °F")
+    println("ผลลัพธ์: $celsius °C เท่ากับ ${"%.2f".format(fahrenheitResult)} °F" )
 }
 
 // ฟังก์ชันสำหรับจัดการกระบวนการแปลง Kilometers to Miles ทั้งหมด
@@ -64,10 +85,14 @@ fun convertKilometersToMiles() {
     // ออกจากฟังก์ชัน convertKilometersToMiles() หากข้อมูลผิดพลาด: return
     // kilometers
     // 🚨
-
-
+    val kilometers = input.toDoubleOrNull() ?: run {
+        println("ข้อผิดพลาด: กรุณาป้อนข้อมูลเป็นตัวเลขเท่านั้น")
+        return
+        }
 //🚨    val milesResult = kilometersToMiles(kilometers)
+        val milesResult = kilometersToMiles(kilometers)
 
     // 6. แสดงผลลัพธ์
 //🚨    println("ผลลัพธ์: $kilometers km เท่ากับ ${"%.2f".format(milesResult)} miles")
+        println("ผลลัพธ์: $kilometers km เท่ากับ ${"%.2f".format(milesResult)} miles")
 }
